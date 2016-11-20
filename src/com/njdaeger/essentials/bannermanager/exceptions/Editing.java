ad93f51cedb00f92dabcf79dd9207afc95c7ed1b
NJDaeger <noahdaeger@hotmail.com>
2016-11-20 14:30:40 -0500

    Pushed MainGUI class

---
+++ src/com/njdaeger/essentials/bannermanager/exceptions/Editing.java
@@ -0,0 +1,12 @@
+package com.njdaeger.essentials.bannermanager.exceptions;
+
+public class Editing extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public Editing() {
+		super("Player is in edit mode.");
+	}
+	
+
+}
---
+++ src/com/njdaeger/essentials/bannermanager/exceptions/MetadataException.java
@@ -0,0 +1,10 @@
+package com.njdaeger.essentials.bannermanager.exceptions;
+
+public class MetadataException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public MetadataException() {
+		super("Invalid object data.");
+	}
+}
