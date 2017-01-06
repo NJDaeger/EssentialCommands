4007965a55ef0a28c116c3e40474f198e37435a1
NJDaeger <noahdaeger@hotmail.com>
2017-01-05 22:20:49 -0500

    Another test on compiling

--- src/com/njdaeger/java/essentials/listeners/events/MoveEvent.java
+++ src/com/njdaeger/java/essentials/listeners/events/MoveEvent.java
@@ -7,17 +7,17 @@ import com.njdaeger.java.Groups;
 import com.njdaeger.java.configuration.controllers.PlayerConfig;
 
 public class MoveEvent {
-	
+
 	public void whenAfk(PlayerMoveEvent e) {
 		Location loc = e.getTo();
-		Location afk = Groups.afkloc.getOrDefault(e.getPlayer().getName(), e.getPlayer().getLocation());
-		if (PlayerConfig.getConfig(e.getPlayer()).isAfk() == true){
-			if (afk.distance(loc) >= 3) {
+		Location a = Groups.afkloc.get(e.getPlayer().getName());
+		if (PlayerConfig.getConfig(e.getPlayer()).isAfk() == true) {
+			if (a.distance(loc) >= 3) {
 				PlayerConfig.getConfig(e.getPlayer()).setAfk();
 				return;
 			}
 		}
 		return;
 	}
-	
+
 }
