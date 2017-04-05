52a3011793b5f29464b1dff389f60a17db545434
NJDaeger <noahdaeger@hotmail.com>
2017-04-04 21:49:13 -0400

    Edits

--- NJCommands/com/njdaeger/java/njcommands/listeners/events/CommandEvent.java
+++ NJCommands/com/njdaeger/java/njcommands/listeners/events/CommandEvent.java
@@ -15,13 +15,15 @@ import com.njdaeger.java.enums.Permission;
 import com.njdaeger.java.wrapper.User;
 
 public class CommandEvent {
-
-	/*private AfkCommand afkCommand = new AfkCommand();
-	private MessageCommand messageCommand = new MessageCommand();
-	private ReplyCommand replyCommand = new ReplyCommand();
-	private BroadcastCommand broadcastCommand = new BroadcastCommand();
-	private VanishCommand vanishCommand = new VanishCommand();*/
-
+	
+	/*
+	 * private AfkCommand afkCommand = new AfkCommand(); private MessageCommand
+	 * messageCommand = new MessageCommand(); private ReplyCommand replyCommand
+	 * = new ReplyCommand(); private BroadcastCommand broadcastCommand = new
+	 * BroadcastCommand(); private VanishCommand vanishCommand = new
+	 * VanishCommand();
+	 */
+	
 	public void whenAfk(PlayerCommandPreprocessEvent e) {
 		User user = Core.getUser(e.getPlayer());
 		String message = e.getMessage().toLowerCase();
@@ -31,20 +33,22 @@ public class CommandEvent {
 			}
 			user.setAfk(false);
 		}
-
-		/*if (getAliases(afkCommand).contains(message) || afkCommand.getName().equalsIgnoreCase(message) || getAliases(
-				broadcastCommand).contains(message) || messageCommand.getName().equalsIgnoreCase(message) || getAliases(
-						messageCommand).contains(message) || replyCommand.getName().equalsIgnoreCase(message)
-				|| getAliases(replyCommand).contains(message) || broadcastCommand.getName().equalsIgnoreCase(message)
-				|| getAliases(vanishCommand).contains(message) || vanishCommand.getName().equalsIgnoreCase(message)) {
-			if (user.isAfk()) {
-				return;
-			} else {
-				user.setAfk(false);
-			}
-		}*/
+		
+		/*
+		 * if (getAliases(afkCommand).contains(message) ||
+		 * afkCommand.getName().equalsIgnoreCase(message) || getAliases(
+		 * broadcastCommand).contains(message) ||
+		 * messageCommand.getName().equalsIgnoreCase(message) || getAliases(
+		 * messageCommand).contains(message) ||
+		 * replyCommand.getName().equalsIgnoreCase(message) ||
+		 * getAliases(replyCommand).contains(message) ||
+		 * broadcastCommand.getName().equalsIgnoreCase(message) ||
+		 * getAliases(vanishCommand).contains(message) ||
+		 * vanishCommand.getName().equalsIgnoreCase(message)) { if
+		 * (user.isAfk()) { return; } else { user.setAfk(false); } }
+		 */
 	}
-
+	
 	public void whenBlocked(PlayerCommandPreprocessEvent e) {
 		String d = e.getMessage().replace("/", "");
 		String c = "";
@@ -75,31 +79,31 @@ public class CommandEvent {
 			return;
 		}
 	}
-
+	
 	public void checkReload(PlayerCommandPreprocessEvent e) {
 		if (e.getMessage().startsWith("rl") || e.getMessage().startsWith("reload")) {
 			Core.setReloading(true);
 		}
 		return;
 	}
-
+	
 	public void checkServerReload(ServerCommandEvent e) {
 		if (e.getCommand().startsWith("rl") || e.getCommand().startsWith("reload")) {
 			Core.setReloading(true);
 		}
 		return;
 	}
-
+	
 	public void whenSpied(PlayerCommandPreprocessEvent e) {
 		String string[] = e.getMessage().split(" ");
 		if (Core.getConf().isSpiedCommand(string[0].substring(1, string[0].length()))) {
 			for (User user : Core.getSpyingUsers()) {
-				user.sendMessage(ChatColor.RED + "[SocialSpy] " + ChatColor.GRAY + e.getPlayer().getName()
-						+ ChatColor.ITALIC + ChatColor.GRAY + "/" + e.getMessage());
+				user.sendMessage(ChatColor.RED + "[SocialSpy] " + ChatColor.GRAY + Core.getUser(e.getPlayer())
+				.getNickname() + ": " + ChatColor.ITALIC + ChatColor.GRAY + e.getMessage());
 			}
 		}
 	}
-
+	
 	private List<String> getAliases(String command) {
 		String[] aliases = Core.getRegisteredCommand(command).getAliases();
 		return Arrays.asList(aliases);
